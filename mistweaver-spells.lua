63abe8089040156a75df3c97b0118d64d69c0b4b
Dizaztuh <JoshM_300@yahoo.com>
2023-05-05 16:58:40 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -116,9 +116,9 @@ end)
 
 -- Callback for Tiger Palm
 tigerPalm:Callback(function()
-    -- If Tiger Palm is castable and both Rising Sun Kick and Blackout Kick are on cooldown, then cast Tiger Palm.
-    -- This ensures that Tiger Palm is cast only when both Rising Sun Kick and Blackout Kick are on cooldown.
-    if tigerPalm:Castable(target) and risingSunKick.cd > 0 and blackoutKick.cd > 0 then
+    -- Check if Tiger Palm is castable and if player does not have the "Alpha Tiger" buff
+    -- If this is the case, cast Tiger Palm
+    if tigerPalm:Castable(target) and not player.buff("Alpha Tiger") then
         -- Cast Tiger Palm on the target.
         tigerPalm:Cast(target)
         return
