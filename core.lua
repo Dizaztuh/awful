d8042eb7bbeb804d50befe172779708101e6e7e3
Dizaztuh <JoshM_300@yahoo.com>
2023-05-04 21:43:49 -0400

    Update core.lua

--- core.lua
+++ core.lua
@@ -2,5 +2,5 @@ local Unlocker, awful, project = ...
 
 awful.DevMode = true
 
-project.monk = {}
+project.monk.mistweaver = {}
 project.monk.mistweaver = awful.Actor:New({ spec = 2, class = "monk" })
