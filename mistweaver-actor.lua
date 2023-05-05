b94dfc82f5141690def847bf58a3db3d88200f30
Dizaztuh <132525460+Dizaztuh@users.noreply.github.com>
2023-05-05 10:49:08 -0400

    Add files via upload

---
+++ mistweaver-actor.lua
@@ -0,0 +1,23 @@
+-- Import the necessary modules and get the mistweaver monk profile
+local Unlocker, awful, project = ...
+local mistweaver = project.monk.mistweaver
+
+print("Sisterfister Loaded and ready to fist!")
+
+-- Initialize the mistweaver monk profile
+mistweaver:Init(function()
+    -- Check if there is an enemy target
+    if target.enemy then
+        player.face(target)
+        StartAttack()
+        healingElixir()
+        diffuseMagic()
+        dampenHarm()
+        fortifyingBrew()
+        faelineStomp()
+        rotation()
+        legSweep()
+        paralyze()
+        touchOfDeath()
+    end
+end)
