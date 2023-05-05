98729cfd26ba98f371c8ca18133ec58c2c19a2b0
Dizaztuh <JoshM_300@yahoo.com>
2023-05-05 01:58:41 -0400

    Update mistweaver-actor.lua

--- mistweaver-actor.lua
+++ mistweaver-actor.lua
@@ -4,31 +4,20 @@ local mistweaver = project.monk.mistweaver
 
 print("Sisterfister Loaded and ready to fist!")
 
--- Define the list of spells to cast
-local spellList = {
-    healingElixir,
-    dampenHarm,
-    fortifyingBrew,
-    faelineStomp,
-    tigerPalm,
-    blackoutKick,
-    risingSunKick,
-    legSweep,
-    touchOfDeath
-}
-
 -- Initialize the mistweaver monk profile
 mistweaver:Init(function()
     -- Check if there is an enemy target
     if target.enemy then
         player.face(target)
         StartAttack()
-        
-        -- Loop through the list of spells
-        for _, spell in ipairs(spellList) do
-            -- Call the spell function
-            spell()
-        end
+        healingElixir()
+        dampenHarm()
+        fortifyingBrew()
+        faelineStomp()
+        tigerPalm()
+        blackoutKick()
+        risingSunKick()
+        legSweep()
+        touchOfDeath()
     end
 end)
-
