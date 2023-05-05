3a7b5fa95873520554aab7c96f2e0c85d9393043
Dizaztuh <JoshM_300@yahoo.com>
2023-05-04 22:37:05 -0400

    Update mistweaver-actor.lua

--- mistweaver-actor.lua
+++ mistweaver-actor.lua
@@ -1,4 +1,4 @@
-local Unlocker, awful, project = ...
+lua local Unlocker, awful, project = ...
 local mistweaver = project.monk.mistweavr
 
 -- stuff out here only runs once, when the file is first loaded.
@@ -8,6 +8,10 @@ mistweaver:Init(function()
     if target.enemy then
         player.face(target) -- Make the player face the target before attacking
         StartAttack()
+        tigerPalm()
+        blackoutKick()
+        risingSunKick()
+    end
     -- everything in here is running *on every tick*
     -- its goal is to "act" every frame
     -- that's why you will be spammed with this print when you toggle your routine.
