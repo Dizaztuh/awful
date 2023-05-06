d1e00bc9c9a937126134a308d1537431be7da4e5
Dizaztuh <JoshM_300@yahoo.com>
2023-05-06 01:36:25 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -20,7 +20,7 @@ awful.Populate({
     chiTorpedo = Spell(119582),
     faelineStomp = Spell(388193, {heal = true, ranged = true}),
     paralyze = Spell(115078, { stun = true, targeted = true, range = 20 }),
-    legSweep = Spell(119381, { stun = true, targeted = true, range = 6 }),
+    legSweep = Spell(119381, { stun = true, range = 6 }),
     ringOfPeace = Spell(116844, { cc = true }),
     flyingSerpentKick = Spell(101545),
     fortifyingBrew = Spell(115203, { heal = true }),
