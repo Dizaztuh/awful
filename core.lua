54216d43cd667d89b4ce4ec09dd57bea1447bfdf
Dizaztuh <JoshM_300@yahoo.com>
2023-05-06 12:04:11 -0400

    Update core.lua

--- core.lua
+++ core.lua
@@ -2,7 +2,3 @@ local Unlocker, awful, project = ...
 awful.DevMode = true
 project.monk = {}
 project.monk.mistweaver = awful.Actor:New({ spec = 2, class = "monk" })
-
--- Load the 'gui.lua' module
-local gui = awful.Load("gui")
-gui.init()  -- Initialize the GUI
