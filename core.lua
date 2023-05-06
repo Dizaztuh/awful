ce67277bc57641c7f20e27800b604fb9a46b1b11
Dizaztuh <JoshM_300@yahoo.com>
2023-05-06 04:28:42 -0400

    Update core.lua

--- core.lua
+++ core.lua
@@ -1,6 +1,4 @@
 local Unlocker, awful, project = ...
-local mistweaverUI = require("gui")
-local gui = require("gui")(Unlocker)
 awful.DevMode = true
 project.monk = {}
 project.monk.mistweaver = awful.Actor:New({ spec = 2, class = "monk" })
