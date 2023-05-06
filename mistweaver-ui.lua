67f8bea7afb89f9a085486499d40862a26b4e653
Dizaztuh <JoshM_300@yahoo.com>
2023-05-06 03:01:26 -0400

    Update mistweaver-ui.lua

--- mistweaver-ui.lua
+++ mistweaver-ui.lua
@@ -4,120 +4,73 @@ local UI = awful.UI
 
 -- Other mistweaver code
 
--- Create the UI frame and set properties
-local mistweaverFrame = UI:Create("Frame")
-mistweaverFrame:SetSize(400, 350)
-mistweaverFrame:SetTitle("Mistweaver Settings")
-mistweaverFrame:SetPoint("CENTER")
-mistweaverFrame:EnableMouse(true)
-mistweaverFrame:SetMovable(true)
-mistweaverFrame:RegisterForDrag("LeftButton")
-mistweaverFrame:SetScript("OnDragStart", mistweaverFrame.StartMoving)
-mistweaverFrame:SetScript("OnDragStop", mistweaverFrame.StopMovingOrSizing)
-mistweaverFrame:SetBackdropColor(0, 0, 0, 1)
-mistweaverFrame:SetBackdropBorderColor(0, 1, 0, 1)
+-- Create Mistweaver group with custom colors
+local mistweaverColor = {0, 1, 0, 1}
+local mistweaverGroup = UI:Group({
+    name = "Mistweaver",
+    title = {"Sister", "Fister"},
+    colors = {
+        title = {awfulCream, mistweaverColor},
+        accent = mistweaverColor,
+    }
+})
 
-local function createLabel(parent, text, x, y)
-    local label = parent:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
-    label:SetPoint("TOPLEFT", x, y)
-    label:SetText(text)
-    label:SetTextColor(0, 1, 0, 1)
-    return label
-end
+--! SPELL TABS !--
+local healingTab = mistweaverGroup:Tab("Healing")
+local crowdControlTab = mistweaverGroup:Tab("Crowd Control")
+local offensiveTab = mistweaverGroup:Tab("Offensive")
+local defensiveTab =mistweaverGroup:Tab("Defensive")
 
--- Create labels and sliders
-local healingElixirLabel = createLabel(mistweaverFrame, "Healing Elixir HP%:", 10, -30)
-local healingElixirSlider = UI:Create("Slider", mistweaverFrame)
-healingElixirSlider:SetPoint("TOPLEFT", 10, -50)
-healingElixirSlider:SetRange(1, 100)
-healingElixirSlider:SetValue(55)
-healingElixirSlider:SetWidth(200)
-healingElixirSlider:SetScript("OnValueChanged", function(self, value)
-    -- Add logic to update the healingElixir.hp value
-end)
+--! HEALING TAB !--
+healingTab:Slider({
+    name = "Healing Elixir HP%",
+    range = {1, 100},
+    default = 55,
+    var = "healingElixirHp",
+})
 
-local lifeCocoonLabel = createLabel(mistweaverFrame, "Life Cocoon HP%:", 10, -80)
-local lifeCocoonSlider = UI:Create("Slider", mistweaverFrame)
-lifeCocoonSlider:SetPoint("TOPLEFT", 10, -100)
-lifeCocoonSlider:SetRange(1, 100)
-lifeCocoonSlider:SetValue(50)
-lifeCocoonSlider:SetWidth(200)
-lifeCocoonSlider:SetScript("OnValueChanged", function(self, value)
-    -- Add logic to update the lifeCocoon.hp value
-end)
+healingTab:Slider({
+    name = "Life Cocoon HP%",
+    range = {1, 100},
+    default = 50,
+    var = "lifeCocoonHp",
+})
 
-local fortifyingBrewLabel = createLabel(mistweaverFrame, "Fortifying Brew HP%:", 10, -130)
-local fortifyingBrewSlider = UI:Create("Slider", mistweaverFrame)
-fortifyingBrewSlider:SetPoint("TOPLEFT", 10, -150)
-fortifyingBrewSlider:SetRange(1, 100)
-fortifyingBrewSlider:SetValue(40)
-fortifyingBrewSlider:SetWidth(200)
-fortifyingBrewSlider:SetScript("OnValueChanged", function(self, value)
-    -- Add logic to update the fortifyingBrew.hp value
-end)
+defensiveTab:Slider({
+    name = "Fortifying Brew HP%",
+    range = {1, 100},
+    default = 40,
+    var = "fortifyingBrewHp",
+})
 
--- Create labels and toggles
-local legSweepLabel = createLabel(mistweaverFrame, "Leg Sweep Enabled:", 220, -30)
-local legSweepToggle = UI:Create("CheckButton", mistweaverFrame)
-legSweepToggle:SetPoint("TOPLEFT", 220, -50)
-legSweepToggle:SetScript("OnClick", function(self)
-    -- Add logic to enable/disable Leg Sweep based on the toggle state
-end)
+--! CROWD CONTROL TAB !--
+crowdControlTab:Checkbox({
+    name = "Leg Sweep Enabled",
+    default = true,
+    var = "legSweepEnabled",
+})
 
-local paralyzeLabel = createLabel(mistweaverFrame, "Paralyze Enabled:", 220, -80)
-local paralyzeToggle = UI:Create("CheckButton", mistweaverFrame)
-paralyzeToggle:SetPoint("TOPLEFT", 220, -100)
-paralyzeToggle:SetScript("OnClick", function(self)
-    -- Add logic to enable/disable Paralyze based on the toggle state
-end)
+crowdControlTab:Checkbox({
+    name = "Paralyze Enabled",
+    default = true,
+    var = "paralyzeEnabled",
+})
 
-local faelineStompLabel = createLabel(mistweaverFrame, "Faeline Stomp Enabled:", 220, -130)
-local faelineStompToggle = UI:Create("CheckButton", mistweaverFrame)
-faelineStompToggle:SetPoint("TOPLEFT", 220, -150)
-faelineStompToggle:SetScript("OnClick", function(self)
-    -- Add logic to enable/disable Faeline Stomp based on the toggle state
-end)
+--! OFFENSIVE TAB !--
+offensiveTab:Checkbox({
+    name = "Faeline Stomp Enabled",
+    default = true,
+    var = "faelineStompEnabled",
+})
 
-local dampenHarmLabel = createLabel(mistweaverFrame, "Dampen Harm Enabled:", 220, -180)
-local dampenHarmToggle = UI:Create("CheckButton", mistweaverFrame)
-dampenHarmToggle:SetPoint("TOPLEFT", 220, -200)
-dampenHarmToggle:SetScript("OnClick", function(self)
-    -- Add logic to enable/disable Dampen Harm based on the toggle state
-end)
+defensiveTab:Checkbox({
+    name = "Dampen Harm Enabled",
+    default = true,
+    var = "dampenHarmEnabled",
+})
 
-local touchOfDeathLabel = createLabel(mistweaverFrame, "Touch of Death Enabled:", 220, -230)
-local touchOfDeathToggle = UI:Create("CheckButton", mistweaverFrame)
-touchOfDeathToggle:SetPoint("TOPLEFT", 220, -250)
-touchOfDeathToggle:SetScript("OnClick", function(self)
-    -- Add logic to enable/disable Touch of Death based on the toggle state
-end)
-
--- Make the frame draggable and hide it initially
-mistweaverFrame:SetScript("OnMouseDown", function(self, button)
-    if button == "LeftButton" and not self.isMoving then
-        self:StartMoving()
-        self.isMoving = true
-    end
-end)
-
-mistweaverFrame:SetScript("OnMouseUp", function(self, button)
-    if button == "LeftButton" and self.isMoving then
-        self:StopMovingOrSizing()
-        self.isMoving = false
-    end
-end)
-
-mistweaverFrame:Hide()
-
--- Create a minimap button to toggle the frame visibility
-local minimapButton = UI:Create("MinimapButton")
-minimapButton:SetIcon("Interface\\Icons\\ability_monk_mistweaver_spec")
-minimapButton:SetScript("OnClick", function(self, button)
-    if button == "LeftButton" then
-        if mistweaverFrame:IsVisible() then
-            mistweaverFrame:Hide()
-        else
-            mistweaverFrame:Show()
-        end
-    end
-end)
+offensiveTab:Checkbox({
+    name = "Touch of Death Enabled",
+    default = true,
+    var = "touchOfDeathEnabled",
+})
