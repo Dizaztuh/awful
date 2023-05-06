0bee977d94f5c445fd2387da5f5243a98db78032
Dizaztuh <JoshM_300@yahoo.com>
2023-05-06 00:37:54 -0400

    Update mistweaver-actor.lua

--- mistweaver-actor.lua
+++ mistweaver-actor.lua
@@ -8,7 +8,6 @@ print("Sisterfister Loaded and ready to fist!")
 mistweaver:Init(function()
     -- Check if there is an enemy target
     if target.enemy then
-        StartAttack()
         faelineStomp()
         risingSunKick("prio")
         tigerPalm()
