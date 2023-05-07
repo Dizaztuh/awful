594084bf8af2bb07d6624d885ac245ea1ae4fbf0
Dizaztuh <JoshM_300@yahoo.com>
2023-05-06 20:27:51 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -202,14 +202,10 @@ risingSunKick:Callback("prio", function(spell)
     end
 end)
 
-touchOfDeath:Callback("prio", function(spell)
-    -- Loop through all enemies within range, something arbitrary like 10 yards
-    awful.enemies.within(10).loop(function(enemy)
-        -- Check if spell is Castable and enemy hp is less than 15%  - LESS THAN due to the spell tooltip being "under 15% health"
-        if touchOfDeath:Castable(enemy) and enemy.hp < 15 then
-            -- Cast Touch of Death on the enemy
-            touchOfDeath:Cast(enemy)
-            return true -- exit the loop after casting the spell
-        end
-    end)
+local eventFrame = CreateFrame("Frame")
+eventFrame:RegisterEvent("PLAYER_LOGIN")
+eventFrame:SetScript("OnEvent", function(self, event)
+    if event == "PLAYER_LOGIN" then
+        mistweaver:OnInitialize()
+    end
 end)
