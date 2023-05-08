93bf2c909d3d3f4078385ac7bbbc2a7f238e703d
Dizaztuh <JoshM_300@yahoo.com>
2023-05-07 20:48:27 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -19,7 +19,7 @@ awful.Populate({
     chiTorpedo = Spell(119582),
     faelineStomp = Spell(388193, {heal = true}),
     paralyze = Spell(115078, { stun = true, targeted = true, range = 25 }),
-    legSweep = Spell(119381, { effect = "physical", stun = true, range = 10 }),
+    legSweep = Spell(119381, { effect = "physical", stun = true }),
     ringOfPeace = Spell(116844, { ignoreCasting = true, ignoreChanneling = true, alwaysFace = true, }),
     flyingSerpentKick = Spell(101545),
     fortifyingBrew = Spell(115203, { heal = true }),
@@ -400,7 +400,7 @@ end)
 -- Create a callback for the Leg Sweep ability
 legSweep:Callback(function(spell)
     -- Get the number of players in range
-    local playersInRange = enemies.around(player, 10)   
+    local playersInRange = enemies.around(player, 8)   
     -- Check if the spell is castable on the target
     if legSweep:Castable(target) then
         -- If there are 2 or more enemies around the player within a range of 6 yards, cast Leg Sweep on the target
