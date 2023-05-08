a9dd5f628c771a6c9f90968a70daed1f6fa73f38
Dizaztuh <JoshM_300@yahoo.com>
2023-05-07 21:15:47 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -396,49 +396,6 @@ diffuseMagic:Callback(function(spell)
     end
 end)
 
--- Create a callback for the Leg Sweep ability
-legSweep:Callback(function(spell)
-    -- Use the Awful framework to loop through enemies around the player within a range of 8 yards
-    local enemiesInRange = awful.enemies.around(player, 8)
-
-    -- Define a function to check if a friend with low HP is within range of an enemy
-    local function lowHpFriendInRange()
-        local foundLowHpFriend = false
-        awful.fgroup.loop(function(friend)
-            if friend.hp < 40 then
-                foundLowHpFriend = awful.enemies.around(friend, 8) >= 1
-            end
-        end)
-        return foundLowHpFriend
-    end
-
-    -- Check if the spell is castable on the target
-    if legSweep:Castable(target) then
-        -- If the player's HP is below 45%, cast Leg Sweep on the target
-        if player.hp < 45 then
-            legSweep:Cast(target)
-            awful.alert({
-                message="Casted Leg Sweep!",
-                texture=119381,
-            })
-        -- If there are 2 or more enemies around the player within a range of 8 yards, cast Leg Sweep on the target
-        elseif enemiesInRange > 1 then
-            legSweep:Cast(target)
-            awful.alert({
-                message="Casted Leg Sweep!",
-                texture=119381,
-            })
-        -- If a friend is below 40% HP and there's at least one enemy in range, cast Leg Sweep on the target
-        elseif lowHpFriendInRange() then
-            legSweep:Cast(target)
-            awful.alert({
-                message="Casted Leg Sweep!",
-                texture=119381,
-            })
-        end
-    end
-end)
-
 
 dampenHarm:Callback(function(spell)
     if player.hp <= 65 then -- check if the player's hp is at or below 60%
