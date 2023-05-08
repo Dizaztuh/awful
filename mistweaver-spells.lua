2bd3cc733303b70be55e5797517b6f78d88e04c8
Dizaztuh <JoshM_300@yahoo.com>
2023-05-07 22:36:43 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -403,15 +403,24 @@ end)
 -- Create a callback for the Leg Sweep ability
 legSweep:Callback(function(spell)
     -- Get the number of players in range
-    local playersInRange = enemies.around(player, 10)   
+    local playersInRange = enemies.around(player, 6)   
     -- Check if the spell is castable on the target
     if legSweep:Castable(target) then
         -- If there are 2 or more enemies around the player within a range of 6 yards, cast Leg Sweep on the target
         if playersInRange > 1 then
             return legSweep:Cast(target)
+            awful.alert({
+                message="Casted Leg Sweep!", 
+                texture=119381,
+                })
         -- If the player's HP is below 45%, cast Leg Sweep on the target
         elseif player.hp < 45 and playersInRange <= 1 then
             return legSweep:Cast(target)
+            if totemList[totem.name] then
+                awful.alert({
+                    message="Casted Leg Sweep!", 
+                    texture=119381,
+                    })
         end
     end
     -- Add the new condition for Leg Sweep
@@ -419,6 +428,10 @@ legSweep:Callback(function(spell)
         -- Check if at least 1 enemy is within range and friend's HP is 40% or lower
         if playersInRange >= 1 and friend.hp <= 40 then
             return legSweep:Cast(target)
+            awful.alert({
+                message="Casted Leg Sweep!", 
+                texture=119381,
+                })
         end
     end)
 end)
