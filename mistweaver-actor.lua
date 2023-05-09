ab483d35a1dc1ba2dd3cf69cc181f5cf70553227
Dizaztuh <JoshM_300@yahoo.com>
2023-05-09 18:20:44 -0400

    Update mistweaver-actor.lua

--- mistweaver-actor.lua
+++ mistweaver-actor.lua
@@ -32,5 +32,25 @@ mistweaver:Init(function()
         diffuseMagic()
         touchOfDeath()
         stompTotems()
+        awful.onUpdate(function()
+            awful.triggers.track(function(trigger, uptime)
+                if player.combat then
+                    local id = trigger.id
+                    if not id then return end
+                    local reacts = ROPDROP[id]
+                    if not reacts then return end
+                    local x, y, z = trigger.position()
+                    if x and y and z then
+                        if uptime < 0.4 or uptime > 2.1 then return end
+                        if trigger.creator.friend then return end  
+                        if not player.losCoordsLiteral(x, y, z) then return end
+                        if ringOfPeace:AoECast(x,y,z) then
+                            awful.alert("Ring of Peace Dropped!", 116844)
+                            return true
+                        end
+                    end
+                end
+            end)
+        end)
     end
 end)
