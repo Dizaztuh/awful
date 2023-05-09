dfe8bc8b814327e6924f9ed646b47da4a710a4d7
Dizaztuh <JoshM_300@yahoo.com>
2023-05-08 21:15:34 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -184,24 +184,25 @@ ROPDROP = {
     [255647] = true, -- The Hunt
     }
     
-    awful.triggers.track(function(trigger, uptime)
-        if player.combat then
-            local id = trigger.id
-            if not id then return end
-            local reacts = ROPDROP[id]
-            if not reacts then return end
-            local x, y, z = trigger.position()
-            if x and y and z then
-                if uptime < 0.4 or uptime > 2.1 then return end
-                if trigger.creator.friend then return end  
-                if not player.losCoordsLiteral(x, y, z) then return end
-                if ringOfPeace:AoECast(x,y,z) then
-                    awful.alert("Ring of Peace Dropped!", 116844)
-                    return true
+    ringOfPeace:Callback(function(spell)
+        awful.triggers.track(function(trigger, uptime)
+            if player.combat then
+                local id = trigger.id
+                if not id then return end
+                local reacts = ROPDROP[id]
+                if not reacts then return end
+                local x, y, z = trigger.position()
+                if x and y and z then
+                    if uptime < 0.4 or uptime > 2.1 then return end
+                    if trigger.creator.friend then return end  
+                    if not player.losCoordsLiteral(x, y, z) then return end
+                    if spell:AoECast(x,y,z) then
+                        awful.alert("Ring of Peace Dropped!", 116844)
+                        return true
+                    end
                 end
-            end
-        end
-      end)
+            end)
+          end)
 
 -- Stomp totems function
 function stompTotems()
