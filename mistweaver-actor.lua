964a1c742802dca6046b8a8932de1d04c72fa35f
Dizaztuh <JoshM_300@yahoo.com>
2023-05-11 18:38:21 -0400

    Update mistweaver-actor.lua

--- mistweaver-actor.lua
+++ mistweaver-actor.lua
@@ -41,29 +41,44 @@ local enemyBuffTable = {
     [62618] = true, -- Barrier
     [198838] = true, -- Earthen
 }
--- Add the event with a callback function
-awful.events.add("COMBAT_LOG_EVENT_UNFILTERED", function(_, event, _, sourceGUID, _, _, destGUID, _, _, _, _, spellID)
-    -- Check if the event is SPELL_AURA_APPLIED
-    if event == "SPELL_AURA_APPLIED" then
-        -- Check if the spell is in the enemyBuffTable
-        if enemyBuffTable[spellID] then
-            -- Get the enemy with the matching GUID
-            local enemy = awful.enemies.getByGUID(destGUID)
-            if enemy then
-                -- Get the enemy's position
-                local x, y, z = enemy.position()
-                if x and y and z then
-                    awful.alert({
-                        message="Casting Ring of Peace on " .. enemy.name,
-                        texture=116844,
-                    })
-                    -- Cast Ring of Peace at the enemy's position
-                    ringOfPeace:AoECast(x, y, z)
+-- Sample buff table with spell IDs
+local enemyBuffTable = {
+    [98765] = true, -- Power Word: Barrier
+    [54321] = true, -- Earthen Wall
+}
+
+local function eventHandler(self, event, ...)
+    if event == "COMBAT_LOG_EVENT_UNFILTERED" then
+        local _, subEvent, _, sourceGUID, _, _, destGUID, _, _, _, _, spellID = CombatLogGetCurrentEventInfo()
+
+        -- Check if the subEvent is SPELL_AURA_APPLIED
+        if subEvent == "SPELL_AURA_APPLIED" then
+            -- Check if the spell is in the enemyBuffTable
+            if enemyBuffTable[spellID] then
+                -- Get the enemy with the matching GUID
+                local enemy = awful.enemies.getByGUID(destGUID)
+                if enemy then
+                    -- Get the enemy's position
+                    local x, y, z = enemy.position()
+                    if x and y and z then
+                        awful.alert({
+                            message="Casting Ring of Peace on " .. enemy.name,
+                            texture=116844,
+                        })
+                        -- Cast Ring of Peace at the enemy's position
+                        ringOfPeace:AoECast(x, y, z)
+                    end
                 end
             end
         end
     end
-end)
+end
+
+-- Create a frame and register the event
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+frame:SetScript("OnEvent", eventHandler)
+
         end
     end
 end)
