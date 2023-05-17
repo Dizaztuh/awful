7bbb07e0ff1cd761ac65ddb48e647253c95e13f1
Dizaztuh <JoshM_300@yahoo.com>
2023-05-16 23:10:14 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -825,7 +825,7 @@ end)
 -- Create a callback for the Paralyze ability
 paralyze:Callback(function(spell)
     -- Check if the enemy healer is valid, within paralyze.range, the target's hp is below 40%, the spell is castable on the enemy healer, and the enemy healer is not the player's target
-    if enemyHealer.distance <= paralyze.range and target.hp < 70 and paralyze:Castable(enemyHealer) and not (player.target.guid == enemyHealer.guid) then
+    if enemyHealer.distance <= paralyze.range and target.hp < 70 and paralyze:Castable(enemyHealer) and not (player.target.enemyHealer) then
         -- If the conditions are met, cast Paralyze on the enemy healer
         paralyze:Cast(enemyHealer)
         awful.alert({
