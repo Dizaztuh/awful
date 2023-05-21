58ecbb6829c0acb5039eed202eb9c4076338fbb1
Dizaztuh <JoshM_300@yahoo.com>
2023-05-21 02:37:37 -0400

    disc

---
+++ Discipline/discipline-actor.lua
@@ -0,0 +1,13 @@
+local Unlocker, awful, project = ...
+local mistweaver = project.priest.discipline
+
+print("Dizazter's Discipline Priest Loaded")
+
+discipline:Init(function()
+    if not player.mounted and target.enemy then
+    renew()
+    PwShield()
+    purgeTheWicked()
+    penance()
+    end
+end)
