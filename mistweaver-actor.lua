ec12fff08e70cc445d3cee31d7627f5d31d1233b
Dizaztuh <JoshM_300@yahoo.com>
2023-05-24 13:03:41 -0400

    Update mistweaver-actor.lua

--- mistweaver-actor.lua
+++ mistweaver-actor.lua
@@ -42,3 +42,31 @@ mistweaver:Init(function()
         end
     end
 end)
+
+-- Listen for SPELL_CAST_START events
+awful.onEvent(function(eventInfo, eventType, sourceObject, destObject)
+    -- Unpack the event information
+    local _, subEvent, _, sourceGUID, sourceName, _, _, destGUID, destName, _, _, spellID, spellName, _, _, _, castTime = unpack(eventInfo)
+
+    -- Check if the event is a spell cast start event
+    if subEvent == "SPELL_CAST_START" then
+        -- Check if the spell caster is an enemy and the spell is in the provokeTable
+        if sourceObject.enemy and provokeTable[spellName] then
+            -- Check if the cast target is the player
+            if destObject.isUnit(player) then
+                -- Schedule Provoke to be cast 0.5 seconds before the spell completes
+                awful.timer(castTime - 0.5, function()
+                    if provoke:Castable(sourceObject) then
+                        -- Create an alert
+                        awful.alert({
+                            message = "Casting Provoke on " .. sourceName,
+                            texture = 115546,
+                        })
+                        -- Cast Provoke on the enemy
+                        provoke:Cast(sourceObject)
+                    end
+                end)
+            end
+        end
+    end
+end)
