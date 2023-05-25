030888f1a922cb9cc72f6fe8b66270a166da82fd
Dizaztuh <JoshM_300@yahoo.com>
2023-05-25 17:15:47 -0400

    Update mistweaver-actor.lua

--- mistweaver-actor.lua
+++ mistweaver-actor.lua
@@ -1,12 +1,23 @@
 -- Import the necessary modules and get the mistweaver monk profile
 local Unlocker, awful, project = ...
 local mistweaver = project.monk.mistweaver
+local type = awful.instanceType2
+PVP = type == "pvp" or type == "arena"
+PVE = type == "party" or type == "raid" or type == "scenario" or type == "none"
+INSTANCE = type == "party" or type == "raid" or type == "scenario"
+BATTLEGROUND = type == "pvp"
+ARENA = type == "arena"
+DUNGEON = type == "party"
+RAID = type == "raid"
+SCENARIO = type == "scenario"
+WORLD = type == "none"
 
 print("Sisterfister Loaded!")
 
 -- Initialize the mistweaver monk profile
 mistweaver:Init(function()
-    if not player.mounted and target.enemy then
+    if player.buff("Arena Preparation") or player.buff ("Drink") then return end
+    if not player.mounted and (ARENA or BATTLEGROUND or DUNGEON or RAID) then
             touchOfDeath()
             ringOfPeace()
             castOnClosestEnemy()
@@ -37,7 +48,7 @@ mistweaver:Init(function()
             dampenHarm()
             fortifyingBrew()
             diffuseMagic()
-
+    end
     end
 end)
 
