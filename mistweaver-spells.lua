509f0e9a2c215de6ddb3dbe585aabdaf7b46cff9
Dizaztuh <JoshM_300@yahoo.com>
2023-06-01 10:27:58 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -5,7 +5,7 @@ local Spell = awful.Spell
 local delayLowerBound = 0.4
 local delayUpperBound = 0.6
 local ringOfPeaceTriggeredTime = 0
-local settings = project.settings
+settings = project.settings
 awful.enabled = true
 
 awful.Populate({
