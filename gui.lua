a1b3fd7bfa885263326defcad348064e86fa87f2
Dizaztuh <JoshM_300@yahoo.com>
2023-06-01 19:55:29 -0400

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -81,13 +81,9 @@ healing:Slider({text = "Sphere of Hope", var = "soh", min = 0, max = 100, defaul
 healing:Slider({text = "Restoral", var = "restoral", min = 0, max = 100, default = 37, valueType = "%", tooltip = "Restoral at friend % HP."})
 healing:Slider({text = "Revival", var = "revival", min = 0, max = 100, default = 37, valueType = "%", tooltip = "Restoral at friend % HP."})
 healing:Slider({text = "Healing Elixir", var = "he", min = 0, max = 100, default = 65, valueType = "%", tooltip = "Healing Elixir triggered at % HP."})
-if player.hasTalent(287503) then
 healing:Checkbox({text = "Renewing Mist", var = "rm", default = true, tooltip = "Enable/Disable Auto Instant Proc Renewing Mists"})
 healing:Checkbox({text = "Enveloping Mist", var = "em", default = true, tooltip = "Enable/Disable Auto Instant Proc Enveloping Mists."})
-elseif
-healing:Slider({text = "Renewing Mists", var = "renewing", min = 0, max = 100, default = 37, valueType = "%", tooltip = "Renewing Mists at % HP"})
-healing:Slider({text = "Enveloping Mists", var = "enveloping", min = 0, max = 100, default = 65, valueType = "%", tooltip = "Enveloping Mists at % HP"})
-end
+
 
 -- Misc tab
 local misc = gui:Tab("Misc")
