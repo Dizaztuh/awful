1c621e4d745c96c211c273de6dd620f745360e01
Dizaztuh <JoshM_300@yahoo.com>
2023-06-01 17:36:22 -0400

    gui

--- gui.lua
+++ gui.lua
@@ -69,21 +69,21 @@ offensive:Checkbox({text = "Chi-Ji on Friendly Burst", var = "cjf", default = tr
 
 -- Defensive tab
 local defensive = gui:Tab("Defensive")
-defensive:Slider({text = "Fortifying Brew", var = "fBrew", min = 0, max = 100, step = 1, default = 40, valueType = "%", tooltip = "Fortifying Brew triggered at % HP. (Will trade on enemy Burst!)"})
-defensive:Slider({text = "Transfer Juke", var = "transferJuke", min = 0, max = 100, step = 1, default = 60, valueType = "%", tooltip = "Transfer Juke triggered on stun at % HP."})
+defensive:Slider({text = "Fortifying Brew", var = "fortBrew", min = 0, max = 100, step = 1, default = 40, valueType = "%", tooltip = "Fortifying Brew triggered at % HP. (Will trade on enemy Burst!)"})
 defensive:Slider({text = "Ring of Peace", var = "ringPeace", min = 0, max = 100, step = 1, default = 40, valueType = "%", tooltip = "Ring of Peace trigger on friendly at % HP."})
 defensive:Slider({text = "Dampen Harm", var = "dh", min = 0, max = 100, default = 44, valueType = "%", tooltip = "Dampen Harm triggered at % HP."})
 defensive:Slider({text = "Life Cocoon", var = "lc", min = 0, max = 100, default = 40, valueType = "%", tooltip = "Life Cocoon triggered at % HP."})
 defensive:Slider({text = "Diffuse Magic", var = "dm", min = 0, max = 100, default = 30, valueType = "%", tooltip = "Diffuse Magic triggered at % HP. (Will normally save for Mindgames etc.)"})
-
+defensive:Slider({text = "Transfer", var = "transferJuke", min = 0, max = 100, step = 1, default = 60, valueType = "%", tooltip = "Transfer Juke triggered on stun at % HP."})
 -- Healing tab
 local healing = gui:Tab("Healing")
 healing:Slider({text = "Sphere of Hope", var = "soh", min = 0, max = 100, default = 90, valueType = "%", tooltip = "Sphere of Hope ally at % HP. (Limited 15 second timer check.)"})
 healing:Slider({text = "Restoral", var = "restoral", min = 0, max = 100, default = 37, valueType = "%", tooltip = "Restoral at friend % HP.)"})
 healing:Slider({text = "Revival", var = "revival", min = 0, max = 100, default = 37, valueType = "%", tooltip = "Restoral at friend % HP.)"})
+healing:Slider({text = "Healing Elixir", var = "he", min = 0, max = 100, default = 65, valueType = "%", tooltip = "Healing Elixir triggered at % HP."})
 healing:Checkbox({text = "Renewing Mist", var = "rm", default = true, tooltip = "Enable/Disable Auto Instant Proc Renewing Mists"})
 healing:Checkbox({text = "Enveloping Mist", var = "em", default = true, tooltip = "Enable/Disable Auto Instant Proc Enveloping Mists."})
-healing:Slider({text = "Healing Elixir", var = "he", min = 0, max = 100, default = 65, valueType = "%", tooltip = "Healing Elixir triggered at % HP."})
+
 -- Misc tab
 local misc = gui:Tab("Misc")
 misc:Dropdown({
