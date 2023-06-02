2fe69d026c509b82c5d177c0b89936833df1b6a6
Dizaztuh <JoshM_300@yahoo.com>
2023-06-02 00:24:30 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -221,12 +221,9 @@ manaTea:Callback(function(spell)
 end)
 
 -- Define a variable to remember the last known statue position
-local lastStatuePosition = nil
-
 summonJadeSerpent:Callback(function(spell)
     local statue = nil
     local statueDistanceToPlayer = nil
-    local tolerance = 5  -- Tolerance of 5 yards
 
     -- Loop through all objects to find the statue
     awful.objects.loop(function(obj)
@@ -237,23 +234,12 @@ summonJadeSerpent:Callback(function(spell)
         end
     end)
 
-    -- If we didn't find a statue within 40 yards of the player, cast the spell
+    -- If we didn't find a statue or the statue is farther than 40 yards away from the player, cast the spell
     if not statue or (statueDistanceToPlayer and statueDistanceToPlayer > 40) then
         local x, y, z = player.position()
         spell:AoECast(x, y, z)
-        lastStatuePosition = {x = x, y = y, z = z}
-    elseif statue and statueDistanceToPlayer > tolerance and lastStatuePosition then
-        -- If the statue exists, the player is more than the tolerance distance away from the statue, and the player has moved more than 20 yards away from the last statue position, recast the spell
-        local playerPosition = {x = player.x, y = player.y, z = player.z}
-        local distanceFromLastStatue = ((playerPosition.x - lastStatuePosition.x) ^ 2 + (playerPosition.y - lastStatuePosition.y) ^ 2 + (playerPosition.z - lastStatuePosition.z) ^ 2) ^ 0.5
-        if distanceFromLastStatue > 40 then
-            local x, y, z = player.position()
-            spell:AoECast(x, y, z)
-            lastStatuePosition = {x = x, y = y, z = z}
-        end
     end
 end)
-
 
 invokeYulon:Callback(function(spell)
     -- Loop through all enemy units
