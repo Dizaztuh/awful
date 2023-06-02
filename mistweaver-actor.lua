f989374856d8134f788b358d79501a14872c4936
Dizaztuh <JoshM_300@yahoo.com>
2023-06-02 09:33:49 -0400

    Update mistweaver-actor.lua

--- mistweaver-actor.lua
+++ mistweaver-actor.lua
@@ -2,6 +2,7 @@
 local Unlocker, awful, project = ...
 local mistweaver = project.monk.mistweaver
 settings = project.settings
+local player = awful.player
 local function updateInstanceType()
     local _, instanceType = IsInInstance()
     PVP = instanceType == "pvp" or instanceType == "arena"
