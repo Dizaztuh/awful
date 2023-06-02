51b26318f94935dc4b0bf0b67fde83c2b71360bd
Dizaztuh <JoshM_300@yahoo.com>
2023-06-02 01:55:49 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -799,7 +799,7 @@ enveloping:Callback(function(spell)
 
     -- Initialize a variable to store the friendly unit with the lowest HP
     local lowestHpFriend = nil
-    local lowestHpPercentage = 90
+    local lowestHpPercentage = 100
 
     -- Loop through all friendly units
     awful.fgroup.loop(function(friend)
