63b60f973a945205535419f64c5aba4b3c6467e4
Dizaztuh <JoshM_300@yahoo.com>
2023-06-03 16:05:36 -0400

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -80,7 +80,7 @@ healing:Slider({text = "Revival", var = "revival", min = 0, max = 100, default =
 healing:Slider({text = "Healing Elixir", var = "he", min = 0, max = 100, default = 65, valueType = "%", tooltip = "Healing Elixir triggered at % HP."})
 
 local healing2 = gui:Tab("Caster Healing")
-healing2:Slider({text = "Enveloping Mist (Caster)", var = "emist", min = 0, max = 100, default = 90, valueType = "%", tooltip = "Enveloping Mist triggered at % HP while Soothing Mist"})
+healing2:Slider({text = "Enveloping Mist (Caster)", var = "emist", min = 0, max = 100, default = 85, valueType = "%", tooltip = "Enveloping Mist triggered at % HP while Soothing Mist"})
 healing2:Slider({text = "Renewing Mist (Caster)", var = "rmist", min = 0, max = 100, default = 95, valueType = "%", tooltip = "Renewing Mist triggered at % HP."})
 healing2:Slider({text = "Vivify (Caster)", var = "viv", min = 0, max = 100, default = 90, valueType = "%", tooltip = "Vivify triggered at % HP while Soothing Mist."})
 
