6b21b4644091b85a486713d5c5cf03e00817040c
Dizaztuh <JoshM_300@yahoo.com>
2023-06-04 01:50:16 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -867,7 +867,7 @@ vivify:Callback(function(spell)
         end
     end)
     if player.buff(388519) or player.buff(325209) or player.buffStacks(388048) >= 3 then return end
-    if vivify:Castable() and lowestHpFriend and lowestHpFriend.buff("Soothing Mist") and (player.buff(388518) or player.buff(116680)) then
+    if vivify:Castable() and ((lowestHpFriend and lowestHpFriend.buff("Soothing Mist")) or player.buff(388518) or player.buff(116680)) then
         spell:Cast(lowestHpFriend)
     end
     
