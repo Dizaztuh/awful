be961ff80e15f406108a1a8665226e41492a92dc
Dizaztuh <JoshM_300@yahoo.com>
2023-06-09 10:35:45 -0400

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -54,7 +54,7 @@ healing2:Slider({text = "Enveloping Mist (Caster)", var = "emist", min = 0, max 
 healing2:Slider({text = "Renewing Mist (Caster)", var = "rmist", min = 0, max = 100, default = 95, valueType = "%", tooltip = "Renewing Mist triggered at % HP."})
 healing2:Slider({text = "Vivify (Caster)", var = "viv", min = 0, max = 100, default = 90, valueType = "%", tooltip = "Vivify triggered at % HP while Soothing Mist."})
 healing2:Slider({text = "Zen Focus Tea", var = "zft", min = 0, max = 100, default = 60, valueType = "%", tooltip = "Use ZFT at % HP. (If interrupts are available)"})
-healing:Slider({text = "Sheiluns Gift", var = "sheiluns", min = 0, max = 100, default = 60, valueType = "%", tooltip = "Sheiluns Gift triggered at % HP. (and at 8 or more stacks)"})
+healing2:Slider({text = "Sheiluns Gift", var = "sheiluns", min = 0, max = 100, default = 60, valueType = "%", tooltip = "Sheiluns Gift triggered at % HP. (and at 8 or more stacks)"})
 
 -- Misc tab
 local misc = gui:Tab("Misc")
