e1ac9de579b9ba6f6407dfb365fb74bfbfba53c6
Dizaztuh <JoshM_300@yahoo.com>
2023-06-12 20:45:43 -0400

    Update draws.lua

--- draws.lua
+++ draws.lua
@@ -11,40 +11,72 @@ local function isUnitInRange(unit, maxRange)
     return distance <= maxRange
 end
 
+local classColors = {
+    ["WARRIOR"] = {152, 83, 34},       -- Brown
+    ["PALADIN"] = {248, 109, 250},     -- Pink
+    ["PRIEST"] = {255, 255, 255},      -- White
+    ["ROGUE"] = {255, 255, 62},         -- Yellow
+    ["HUNTER"] = {113, 231, 113},        -- Green
+    ["DRUID"] = {255, 130, 0},         -- Orange
+    ["SHAMAN"] = {0, 0, 255},          -- Blue
+    ["MAGE"] = {0, 151, 255},        -- Light Blue
+    ["WARLOCK"] = {120, 0, 255},     -- Light Purple
+    ["DEMONHUNTER"] = {95, 0, 203},    -- Dark Purple
+    ["EVOKER"] = {31, 203, 155},        -- Teal
+    ["DEATHKNIGHT"] = {225, 0, 0},      -- Dark Red
+    ["MONK"] = {0, 255, 142}  -- Brightish Green
+}
+
 Draw(function(draw)
     -- Get the instance type every time Draw is called
     local _, instanceType = IsInInstance()
-   
-    -- Check if the instance type is 'arena'
+
+    -- Check if the instance type is 'arena' and if drawing is enabled in the settings
     if instanceType == "arena" and settings.draws then
         -- Set the width of the lines
         draw:SetWidth(3)
 
         -- Loop through all friends around the player
         awful.friends.loop(function(friend)
+            -- Get the friend's class
+            local class = friend.class2
+
             -- Get the friend's current position (x, y, z coordinates)
             local fx, fy, fz = friend.position()
 
-            -- Check if the friend is within 40 yards of the player
-            if isUnitInRange(friend, 40) then
-                -- Get the line of sight status between the player and the friend
-                local los = player.losOf(friend)
+            -- Get the line of sight status between the player and the friend
+            local los = player.losOf(friend)
 
-                -- Set the color of the line based on line of sight
-                if los then
-                    -- Bright green color when friend is in line of sight
-                    draw:SetColor(0, 255, 0, 100)
+            -- Calculate the distance between the player and the friend
+            local distance = player.distanceTo(friend)
+
+            -- Set the color of the line based on line of sight and distance
+            if los and distance <= 40 then
+                -- Class color when friend is in line of sight and within 40 yards
+                if classColors[class] then
+                    -- Get the class color
+                    local r, g, b = unpack(classColors[class])
+                    draw:SetColor(r, g, b, 200)
                 else
-                    -- Red color when friend is out of line of sight
-                    draw:SetColor(255, 0, 0, 100)
+                    -- Default to white if the class color is not defined
+                    draw:SetColor(255, 255, 255, 200)
                 end
-
-                -- Get the player's current position (x, y, z coordinates)
-                local px, py, pz = player.position()
-
-                -- Draw the line from the player to the friend
-                draw:Line(px, py, pz, fx, fy, fz)
+            else
+                -- Bright red for LOS / ranged friends
+                draw:SetColor(255, 0, 0, 200)  -- This is an example color, adjust RGB values to your liking
+                
             end
+
+            -- Get the player's current position (x, y, z coordinates)
+            local px, py, pz = player.position()
+
+            -- Draw the line from the player to the friend
+            draw:Line(px, py, pz, fx, fy, fz)
         end)
+        
+        -- Draw a light blue circle of radius 8 yards around the player
+        local px, py, pz = player.position()  -- Get the player's current position
+        draw:SetColor(0, 255, 142, 200)   -- Set the color to light blue (RGB: 173, 216, 230)
+        draw:Circle(px, py, pz, 30)  -- Draw the circle
     end
 end)
