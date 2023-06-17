62a7918b3393da8cf5d8c93af460b8da3d59045a
Dizaztuh <JoshM_300@yahoo.com>
2023-06-17 19:42:08 -0400

    g

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -271,6 +271,30 @@ BurstCDS = {
     [262161] = true -- Warbreaker
 }
 
+-- Callback for Chi Wave
+chiWave:Callback(function(spell)
+    local closestEnemy
+    awful.enemies.loop(function(enemy)
+        if player.distance(enemy) <= 5 then
+            closestEnemy = enemy
+        end
+    end)
+    
+    if closestEnemy and spell:Castable(closestEnemy) then
+        return spell:Cast(closestEnemy)
+    end
+
+    local lowestHpFriend
+    awful.friends.loop(function(friend)
+        if not lowestHpFriend or friend.hp < lowestHpFriend.hp then
+            lowestHpFriend = friend
+        end
+    end)
+
+    if lowestHpFriend and spell:Castable(lowestHpFriend) then
+        return spell:Cast(lowestHpFriend)
+    end
+end)
 
 
 roll:Callback(function(spell)
@@ -1324,9 +1348,6 @@ end)
 
 -- Callback for Rising Sun Kick
 risingSunKick:Callback(function(spell)
-    if allEnemies.around(player, 5) > 2 then return end
-    return spell:Cast()
-end
         spell:Cast(target)
 end)
 
