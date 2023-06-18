e8a5a926c46102021134bb204b1f31e94ff32dc3
Dizaztuh <JoshM_300@yahoo.com>
2023-06-18 03:36:24 -0400

    Update mistweaver-spells.lua

--- mistweaver-spells.lua
+++ mistweaver-spells.lua
@@ -621,9 +621,9 @@ end)
 
 -- Stomp totems function
 function stompTotems()
-    awful.totems.stomp(function(totem, uptime)
+    awful.totems.stomp(function(totem)
         -- Only stomp selected totems and within range
-        if totem.id and settings.totemsToStomp[totem.id] and totem.distance <= tigerPalm.range then
+        if totem.id and settings.totemsToStomp[totem.id] and totem.distance <= 5 then
             awful.alert({
                 message="Stomping a totem.",  
                 texture=100780,
